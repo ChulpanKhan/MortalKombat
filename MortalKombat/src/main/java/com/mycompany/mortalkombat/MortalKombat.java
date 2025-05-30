8abad9061f471a9c39ce04213e68fad285b0d9e8
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-05-30 23:31:29 +0300

    main

---
+++ MortalKombat/src/main/java/com/mycompany/mortalkombat/MortalKombat.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mortalkombat;
+
+import com.mycompany.mortalkombat.Forms.MenuFrame;
+
+/**
+ *
+ * @author MyHuawei
+ */
+public class MortalKombat {
+
+    public static void main(String[] args) {
+        MenuFrame app = new MenuFrame();       
+        app.setVisible(true);
+    }
+}
